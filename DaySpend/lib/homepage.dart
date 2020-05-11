c7c092c3dba19adb54a87cdf077a6fa0889f77ac
danielwse <danieltzy@hotmail.com>
2020-05-11 18:50:36 +0800

    changed background

--- DaySpend/lib/homepage.dart
+++ DaySpend/lib/homepage.dart
@@ -1,41 +1,60 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(  
+        /*
+      appBar: AppBar( 
         backgroundColor: Colors.black,
         title: Text("DaySpend",
         style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
-      ),
-      body: Center(  
-        child: Column(  
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Welcome To DaySpend!",
-            style: TextStyle( 
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              foreground: Paint()
-              ..style = PaintingStyle.fill
-              ..strokeWidth = 3
-              ..color = Colors.blue
-            )
+      ),*/
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+            Colors.blue,
+            Colors.red,
+          ])),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Text(
+              "Welcome To DaySpend!",
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()
+                    ..style = PaintingStyle.fill
+                    ..strokeWidth = 3
+                    ..color = Colors.white),
+              textAlign: TextAlign.center,
             ),
-          new RaisedButton(onPressed: () {},
-          child: new Text("Calender"),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+            margin: EdgeInsets.only(bottom: 50),
           ),
-          new RaisedButton(onPressed: () {},
-          child : new Text("To-Do List"),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+          Container(
+            child: new RaisedButton(
+              onPressed: () {},
+              child: new Text("Calender"),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+          ),
+          Container(
+            child: new RaisedButton(
+              onPressed: () {},
+              child: new Text("To-Do List"),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
           )
-          ],
-        ),)
-      );
-    }
+        ],
+      ),
+    ));
   }
+}
