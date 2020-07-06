501c9b82e530908c51a1f8f09b1bc16eb1544306
jadenwjh <wongjunhong9723@gmail.com>
2020-07-06 10:34:35 +0800

    Update picker.dart

--- DaySpend/lib/planner/picker.dart
+++ DaySpend/lib/planner/picker.dart
@@ -28,81 +28,77 @@ class _PickerButtonState extends State<PickerButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<TaskFunction>(
-      builder: (context, tempDateTimeData, child) {
-        return NiceButton(
-          text: DateFormat('EEEE').format(tempOldDateTime).toString() + " - " + DateFormat('Hm').format(tempOldDateTime).toString(),
-          background: Colors.teal,
-          elevation: 6,
-          fontSize: 14,
-          textColor: Colors.white,
-          width: 160,
-          radius: 30.0,
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                isScrollControlled: true,
-                builder: (context) => SingleChildScrollView(
-                  child: Container(
-                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                    child: Container(
-                      color: Color(0xff757575),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20.0),
-                            topRight: Radius.circular(20.0),
-                          ),
-                        ),
-                        height: MediaQuery.of(context).copyWith().size.height / 2.5,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              margin: EdgeInsets.symmetric(vertical: 20),
-                              height: MediaQuery.of(context).copyWith().size.height / 4,
-                              width: MediaQuery.of(context).copyWith().size.width / 1.5,
-                              child: CupertinoDatePicker(
-                                mode: CupertinoDatePickerMode.dateAndTime,
-                                initialDateTime: tempOldDateTime,
-                                onDateTimeChanged: (DateTime dt) {
-                                  setState(() {
-                                    newDateTime = dt;
-                                  });
-                                },
-                                use24hFormat: true,
-                                minuteInterval: 1,
-                                minimumDate: limit,
-                                maximumDate: limit.add(Duration(days: 7)),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 16),
-                              child: NiceButton(
-                                width: 100,
-                                text: "Confirm",
-                                elevation: 8.0,
-                                radius: 52.0,
-                                fontSize: 16,
-                                textColor: Colors.white,
-                                background: Colors.teal,
-                                onPressed: () {
-                                  tempDateTimeData.changeDateTime(newDateTime);
-                                  tempOldDateTime = newDateTime;
-                                  Navigator.pop(context);
-                                },
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
+    return NiceButton(
+      text: DateFormat('EEEE').format(tempOldDateTime).toString() + " - " + DateFormat('Hm').format(tempOldDateTime).toString(),
+      background: Colors.teal,
+      elevation: 6,
+      fontSize: 14,
+      textColor: Colors.white,
+      width: 160,
+      radius: 30.0,
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          builder: (context) => SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Container(
+                color: Color(0xff757575),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
                     ),
                   ),
+                  height: MediaQuery.of(context).copyWith().size.height / 2.5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        height: MediaQuery.of(context).copyWith().size.height / 4,
+                        width: MediaQuery.of(context).copyWith().size.width / 1.5,
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.dateAndTime,
+                          initialDateTime: tempOldDateTime,
+                          onDateTimeChanged: (DateTime dt) {
+                            setState(() {
+                              newDateTime = dt;
+                            });
+                          },
+                          use24hFormat: true,
+                          minuteInterval: 1,
+                          minimumDate: limit,
+                          maximumDate: limit.add(Duration(days: 7)),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 16),
+                        child: NiceButton(
+                          width: 100,
+                          text: "Confirm",
+                          elevation: 8.0,
+                          radius: 52.0,
+                          fontSize: 16,
+                          textColor: Colors.white,
+                          background: Colors.teal,
+                          onPressed: () {
+                            Provider.of<TaskFunction>(context).changeDateTime(newDateTime);
+                            tempOldDateTime = newDateTime;
+                            Navigator.pop(context);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-            );
-          },
+              ),
+            ),
+          ),
         );
       },
     );
